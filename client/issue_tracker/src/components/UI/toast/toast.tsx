df9423a068c76af7f5871d4d69822286c81738e2
Arjit singh <arjitsingh8@gmail.com>
2023-06-18 22:05:46 +0530

    changes toast structure

--- client/issue_tracker/src/components/UI/toast/toast.tsx
+++ client/issue_tracker/src/components/UI/toast/toast.tsx
@@ -1,9 +1,9 @@
 import { createPortal } from "react-dom";
 import "./style.css"
 
-interface props {
-    text : string
-}
+// interface props {
+//     text : string
+// }
 export default function Toast(){
     return <>
     {
