0f22358858fbff710aa15002c07bdfd04ec2f7e8
Arjit singh <arjitsingh8@gmail.com>
2023-06-25 23:10:02 +0530

    added dragable and dropable elements and fixed draggable bug

--- client/issue_tracker/src/pages/project/dragNdropTasks.tsx
+++ client/issue_tracker/src/pages/project/dragNdropTasks.tsx
@@ -1,12 +1,58 @@
-import "./style.css"
-export default function DragNDropTasks(){
+import { StrictModeDroppable } from "./dropable";
+import "./style.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-    return <>
-        <div className="dndContainer">
-            <div className="dndStatusContainers"></div>
-            <div className="dndStatusContainers"></div>
-            <div className="dndStatusContainers"></div>
-            <div className="dndStatusContainers"></div>
+export default function DragNDropTasks() {
+  const list = [
+    {
+      name: "one",
+      id: 'one',
+    },
+    {
+      name: "two",
+      id: 'two',
+    },
+    {
+      name: "three",
+      id: 'three',
+    },
+  ];
+  return (
+    <>
+      <div className="dndContainer">
+        <div className="dndStatusContainers">
+          <h1>Create Task</h1>
+          <DragDropContext onDragEnd={()=>{}}>
+            <StrictModeDroppable droppableId="items">
+              {(provided) => (
+                <ul {...provided.droppableProps} ref={provided.innerRef} className="items">
+                  {list.map(({ name, id },index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                       {(provided)=>(
+                          <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            {name}
+                            </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </StrictModeDroppable>
+          </DragDropContext>
         </div>
+        <div className="dndStatusContainers">
+          <h1>In Progress</h1>
+        </div>
+        <div className="dndStatusContainers">
+          <h1>Planning</h1>
+        </div>
+        <div className="dndStatusContainers">
+          <h1>Completed</h1>
+        </div>
+      </div>
     </>
+  );
 }
