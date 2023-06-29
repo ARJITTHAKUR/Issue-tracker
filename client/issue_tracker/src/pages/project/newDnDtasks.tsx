dd25a755649343459e163f1cffc47b789bf78bd3
Arjit singh <arjitsingh8@gmail.com>
2023-06-29 22:59:27 +0530

    added drag drop component

--- client/issue_tracker/src/pages/project/newDnDtasks.tsx
+++ client/issue_tracker/src/pages/project/newDnDtasks.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import {DndContext} from '@dnd-kit/core';
-import {useDraggable} from '@dnd-kit/core';
-import {CSS} from '@dnd-kit/utilities';
+import React, { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 
-export default function NewDnD(){
-    const {attributes, listeners, setNodeRef, transform} = useDraggable({
-        id: 'unique-id',
-      });
-      const style = {
-        transform: CSS.Translate.toString(transform),
-      };
-    return (
-        <DndContext>
-   <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      <ul>
-        <li>one</li>
-        <li>two</li>
-      </ul>
+import { useDroppable } from "@dnd-kit/core";
+
+function Droppable(props: any) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: "droppable",
+  });
+  const style = {
+    color: isOver ? "green" : undefined,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
+
+function Draggable(props: any) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: "draggable",
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
     </button>
-        </DndContext>
-      )
+  );
+}
+export default function NewDnD() {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
+  function handleDragEnd(event: any) {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  }
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>
+        <div
+          style={{ width: "200px", height: "200px", border: "1px solid red" }}
+        >
+          {isDropped ? draggableMarkup : "drop here"}
+        </div>
+      </Droppable>
+    </DndContext>
+  );
 }
